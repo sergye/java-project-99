efd60e19fec9067a16d3b2f93b9f63b79610866d
sergye <sergye@gmail.com>
2024-05-15 07:25:52 +0200

    Add UserDTO

---
+++ src/main/java/hexlet/code/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package hexlet.code.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserDTO {
+	private Long id;
+
+	private String username;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String password;
+}
