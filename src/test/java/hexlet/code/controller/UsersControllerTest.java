a103050be39987dacf5a9b0d33be29654cf1a975
sergye <sergye@gmail.com>
2024-05-21 11:37:47 +0200

    Add token to requests

--- src/test/java/hexlet/code/controller/UsersControllerTest.java
+++ src/test/java/hexlet/code/controller/UsersControllerTest.java
@@ -16,17 +16,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 
 @SpringBootTest
@@ -35,6 +42,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UsersControllerTest {
 
     private static final Faker FAKER = new Faker();
+
+    @Autowired
+    private WebApplicationContext wac;
 
     @Autowired
     private MockMvc mockMvc;
@@ -51,19 +61,27 @@ class UsersControllerTest {
     @Autowired
     private DefaultUserProperties defaultUserProperties;
 
+    private JwtRequestPostProcessor token;
+
     private User user;
 
     @BeforeEach
     public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .defaultResponseCharacterEncoding(StandardCharsets.UTF_8)
+                .apply(springSecurity())
+                .build();
+
+        token = jwt().jwt(builder -> builder.subject(defaultUserProperties.getEmail()));
         user = Instancio.of(modelGenerator.getUserModel()).create();
+        userRepository.save(user);
     }
 
     @Test
     public void testIndex() throws Exception {
-        userRepository.save(user);
-        var result = mockMvc.perform(get("/api/users"))
-            .andExpect(status().isOk())
-            .andReturn();
+        var result = mockMvc.perform(get("/api/users").with(jwt()))
+                .andExpect(status().isOk())
+                .andReturn();
 
         var body = result.getResponse().getContentAsString();
         assertThatJson(body).isArray();
@@ -71,9 +89,8 @@ class UsersControllerTest {
 
     @Test
     public void testShow() throws Exception {
-        userRepository.save(user);
 
-        var result = mockMvc.perform(get("/api/users/" + user.getId()))
+        var result = mockMvc.perform(get("/api/users/" + user.getId()).with(token))
             .andExpect(status().isOk())
             .andReturn();
 
@@ -90,10 +107,13 @@ class UsersControllerTest {
     public void testCreate() throws Exception {
         var userData = Instancio.of(modelGenerator.getUserModel()).create();
 
-        mockMvc.perform(post("/api/users")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(om.writeValueAsString(userData)))
-            .andExpect(status().isCreated());
+        var request = post("/api/users")
+                .with(token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(userData));
+
+        mockMvc.perform(request)
+                .andExpect(status().isCreated());
 
         var user = userRepository.findByEmail(userData.getEmail())
             .orElseThrow(() -> new ResourceNotFoundException("User not found"));
@@ -106,13 +126,13 @@ class UsersControllerTest {
 
     @Test
     public void testUpdate() throws Exception {
-        userRepository.save(user);
 
         UserUpdateDTO userData = new UserUpdateDTO();
         userData.setFirstName(JsonNullable.of(FAKER.name().firstName()));
         userData.setEmail(JsonNullable.of(FAKER.internet().emailAddress()));
 
         var request = put("/api/users/" + user.getId())
+            .with(token)
             .contentType(MediaType.APPLICATION_JSON)
             .content(om.writeValueAsString(userData));
 
@@ -128,9 +148,8 @@ class UsersControllerTest {
 
     @Test
     public void testDelete() throws Exception {
-        userRepository.save(user);
 
-        mockMvc.perform(delete("/api/users/" + user.getId()))
+        mockMvc.perform(delete("/api/users/" + user.getId()).with(token))
             .andExpect(status().isNoContent());
 
         assertThat(userRepository.findById(user.getId())).isEmpty();
