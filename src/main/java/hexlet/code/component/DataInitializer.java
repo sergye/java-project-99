883999ed7e09fbff3ccfc487b77d0d3229f36d64
sergye <sergye@gmail.com>
2024-06-04 17:53:59 +0200

    Add DefaultTaskStatuses

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -1,7 +1,10 @@
 package hexlet.code.component;
 
+import hexlet.code.dto.status.TaskStatusCreateDTO;
 import hexlet.code.dto.user.UserCreateDTO;
+import hexlet.code.mapper.TaskStatusMapper;
 import hexlet.code.mapper.UserMapper;
+import hexlet.code.repository.TaskStatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -17,6 +20,12 @@ import lombok.AllArgsConstructor;
 @Component
 @AllArgsConstructor
 public class DataInitializer implements ApplicationRunner {
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private TaskStatusMapper taskStatusMapper;
+
     @Autowired
     private final UserRepository userRepository;
 
@@ -53,6 +62,41 @@ public class DataInitializer implements ApplicationRunner {
                 User user = userMapper.map(userData);
                 userRepository.save(user);
             }
+        }
+    }
+
+    private void createDefaultTaskStatuses() {
+        if (taskStatusRepository.findAll().isEmpty()) {
+            TaskStatusCreateDTO draftDTO = new TaskStatusCreateDTO();
+
+            draftDTO.setName("Draft");
+            draftDTO.setSlug("draft");
+
+            TaskStatusCreateDTO toViewDTO = new TaskStatusCreateDTO();
+
+            toViewDTO.setName("To view");
+            toViewDTO.setSlug("to_review");
+
+            TaskStatusCreateDTO toBeFixedDTO = new TaskStatusCreateDTO();
+
+            toBeFixedDTO.setName("To be fixed");
+            toBeFixedDTO.setSlug("to_be_fixed");
+
+            TaskStatusCreateDTO toPublishDTO = new TaskStatusCreateDTO();
+
+            toPublishDTO.setName("To publish");
+            toPublishDTO.setSlug("to_publish");
+
+            TaskStatusCreateDTO publishedDTO = new TaskStatusCreateDTO();
+
+            publishedDTO.setName("Published");
+            publishedDTO.setSlug("published");
+
+            taskStatusRepository.save(taskStatusMapper.map(draftDTO));
+            taskStatusRepository.save(taskStatusMapper.map(toViewDTO));
+            taskStatusRepository.save(taskStatusMapper.map(toBeFixedDTO));
+            taskStatusRepository.save(taskStatusMapper.map(toPublishDTO));
+            taskStatusRepository.save(taskStatusMapper.map(publishedDTO));
         }
     }
 }
