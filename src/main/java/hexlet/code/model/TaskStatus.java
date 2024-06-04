580928d35f1f8a78651941eaa86235244b5cd526
sergye <sergye@gmail.com>
2024-06-04 11:23:41 +0200

    Add TaskStatus

---
+++ src/main/java/hexlet/code/model/TaskStatus.java
@@ -0,0 +1,46 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.validation.constraints.Size;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "task_statuses")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class TaskStatus implements BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private long id;
+
+	@Size(min = 1)
+	@Column(nullable = false, unique = true)
+	private String name;
+
+	@Size(min = 1)
+	@Column(nullable = false, unique = true)
+	private String slug;
+
+	@CreatedDate
+	private LocalDate createdAt;
+
+	@LastModifiedDate
+	private LocalDate updatedAt;
+}
