f2795f1e36e4d3ee6fc40f95accc42b5b7321a33
sergye <sergye@gmail.com>
2024-06-04 11:39:29 +0200

    Add task status DTOs

--- src/main/java/hexlet/code/dto/status/TaskStatusCreateDTO.java
+++ src/main/java/hexlet/code/dto/status/TaskStatusCreateDTO.java
@@ -1,7 +1,5 @@
 package hexlet.code.dto.status;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
