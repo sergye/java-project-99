9948ebbc142905078de0cde34cd3a34de338124d
sergye <sergye@gmail.com>
2024-06-05 11:22:47 +0200

    Add Task model

---
+++ src/main/java/hexlet/code/model/Task.java
@@ -0,0 +1,49 @@
+package hexlet.code.model;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.validation.constraints.Size;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "tasks")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class Task implements BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private long id;
+
+	@Size(min = 1)
+	private String name;
+
+	private long index;
+
+	private String description;
+
+	@JoinColumn(name = "task_status_id")
+	@ManyToOne
+	private TaskStatus taskStatus;
+
+	@JoinColumn(name = "assignee_id")
+	@ManyToOne
+	private User assignee;
+
+	@CreatedDate
+	private LocalDate createdAt;
+
+	@LastModifiedDate
+	private LocalDate updatedAt;
+}
